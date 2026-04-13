5b98bfc24aeac0a8f8ba5f4c69e4aa6c0a49f66b
pranav-2610 <pranav2arun@gmail.com>
2026-04-13 23:41:17 +0530

    supabase connection

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+import dotenv, os
 
-Database_URL = "mysql+aiomysql://root:@localhost/auth_db"
+
+dotenv.load_dotenv()
+
+Database_URL = f"postgresql+asyncpg://postgres:{os.environ['DB_PASSWORD']}@db.aqkqzcnojcpshtyrrjbi.supabase.co:5432/postgres"
 engine = create_async_engine(Database_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, 
